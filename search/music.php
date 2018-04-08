c40f7ef6419f7380fa0511df059090d5dceeb596
NeatAbacus <grossetempetedeneigede1985@gmail.com>
2018-04-08 11:33:47 -0700

    Add files via upload

--- search/music.php
+++ search/music.php
@@ -1,7 +1,9 @@
 <?php
-require("connexion.php")
+require("connexion.php");
 $conn = mysqli_connect($host, $user, $passwd, $db);
 mysqli_select_db($conn, $db) or die("Unable to select db $db");
+
+echo "<p>Songstuff wewewew</p>";
 
 
 mysqli_close($conn);
