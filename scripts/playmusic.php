f4f7b26f46fdd05e2ee58f6b6c8ff85c639a283b
NeatAbacus <grossetempetedeneigede1985@gmail.com>
2018-04-13 11:23:50 -0300

    Added playmusic.php to query the track's url
    Signed-off-by: NeatAbacus <grossetempetedeneigede1985@gmail.com>

---
+++ scripts/playmusic.php
@@ -0,0 +1,22 @@
+<?php
+require("connexion.php");
+$conn = mysqli_connect($host, $user, $passwd, $db);
+mysqli_select_db($conn, $db) or die("Unable to select db $db");
+
+$val = $_GET['trackid']);
+
+
+$requete = "SELECT * FROM music WHERE id='$val'";
+$results = mysqli_query($conn, $requete);
+
+
+if(($ligne = mysqli_fetch_array($results)) == true)
+{
+	printf("<script type=\"text/javascript\"></script>");
+}
+
+
+
+mysqli_close($conn);
+?>
+
